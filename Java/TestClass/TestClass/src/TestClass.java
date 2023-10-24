a56e1fbcd950c66c2423cd361cf2b7b42db3b979
markdnolan <mark.d.nolan@icloud.com>
2023-10-24 18:41:10 -0400

    Added fist Java exercises

---
+++ Java/TestClass/TestClass/src/TestClass.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+
+public class TestClass extends Object {
+
+    private int x, y = 5;
+
+    public TestClass(int z) {
+        x = z;
+    }
+
+    public void display() {
+        System.out.println("The values of (x,y) are: (" + x + "," + y + ")");
+    }
+
+    public  void main(String[] args) {
+        TestClass test = new TestClass(10);
+        test.display();
+    }
+}
+
